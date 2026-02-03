ff323db11c7a3f49c3cc08a082001ccda8d2b384
rfichi <rdfs.ing@gmail.com>
2026-02-03 15:44:34 +0100

    chore(ci): update auto-pr triggers to include test branches

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="borrowed_book_system",
-    version="0.5.5",
+    version="0.5.6",
     packages=find_packages(exclude=[".venv", "tests*"]),
     install_requires=[],
     description="FastAPI service for managing borrowed books",
